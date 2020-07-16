62e83ac5af52953ddc699cfdedd8a5dae92131c6
roheelislam <56550719+roheelislam@users.noreply.github.com>
2020-07-16 03:57:31 +0100

    Testing & functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-var readline = require('readline')
-var rl = readline.createInterface(process.stdin, process.stdout);
-let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
-
 //TODO: add validation
 //Take user input
 //<Posibility validate>
 //Check list to see if it contains user input value
 //Return True or False
+
+
+var readline = require('readline')
+var rl = readline.createInterface(process.stdin, process.stdout);
+let itemNames = ["PL123456", "PL513872", "PL999999", "PL654321", "PL818181"]
+
+function isProductPresent(product){
+    return itemNames.includes(product)
+}
 
 function inputHandler(answer){
     let result = itemNames.includes(answer)
@@ -20,6 +25,5 @@ function inputHandler(answer){
 
 rl.question("Input your medicine license number: ", inputHandler) 
 
-
-// function inputHandler(){
-// }
+exports.inputHandler = inputHandler
+exports.isProductPresent = isProductPresent
---
+++ src/tests/index.test.js
@@ -0,0 +1,11 @@
+const {isProductPresent, inputHandler} = require("../index.js")
+
+describe("Input handler", () => {
+    test("returns true when product exists", ()=>{
+        expect(isProductPresent("PL123456")).toBe(true)
+    })
+        test("returns false when product doesn't exists", ()=>{
+            expect(isProductPresent("PL123465")).toBe(false)
+        })
+})
+
